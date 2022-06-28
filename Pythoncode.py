bb6a451ee3113b562920e8dcf2ead9c8cb851f61
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-28 15:11:42 +1000

    cool!

--- Pythoncode.py
+++ Pythoncode.py
@@ -5,3 +5,9 @@ letter_to_number = {
   "C": 3,
 }
 
+# Calculate value of word by adding digit values
+def find_value(input: string) -> int:
+  value = 0
+  for letter in input:
+    value += letter_to_number[letter]
+  return value
